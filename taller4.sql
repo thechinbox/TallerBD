bfaf3aec26de5fba3e2e221afe2fba0b57fe7d66
thechinbox <89473920+thechinbox@users.noreply.github.com>
2021-09-08 13:12:01 -0300

    Add files via upload

--- taller4.sql
+++ taller4.sql
@@ -1,19 +1,19 @@
-SELECT personas.nombre FROM propiedades
+SELECT CONCAT(personas.nombre, 'tiene' , COUNT(id_propiedad) , ' propiedades.') FROM propiedades
 INNER JOIN personas ON propiedades.dueno = personas.rut
 GROUP BY personas.nombre
 HAVING count(dueno) > 1
-ORDER BY personas.nombre
 
 SELECT prv.provincia, tpp.tipo_propiedad, too.tipo_operacion, MIN(op.precio)
 	FROM provincias prv, tipos_propiedades tpp, operaciones op
 INNER JOIN tipos_operaciones too ON op.tipo_operacion = too.id_tipooperacion
-GROUP BY (provincia, tipo_propiedad, too.tipo_operacion)
+LEFT JOIN propiedades pro ON op.id_propiedad = pro.id_propiedad
+WHERE pro.id_propiedad IS NULL
+GROUP BY (prv.provincia, tpp.tipo_propiedad, too.tipo_operacion)
 
 SELECT pr.nombre FROM operaciones op
 INNER JOIN personas pr ON op.comprador = pr.rut
 LEFT JOIN tipos_operaciones too ON op.tipo_operacion = too.id_tipooperacion
-WHERE too.tipo_operacion = 'Alquiler'
+WHERE UPPER(too.tipo_operacion) like 'ALQUILER'
 EXCEPT
 SELECT pr.nombre FROM propiedades prp
 INNER JOIN personas pr ON prp.dueno = pr.rut
-
