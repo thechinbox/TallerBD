c0832f99d69e7f6b90b0a811b6c3a805bce8827a
thechinbox <89473920+thechinbox@users.noreply.github.com>
2021-10-13 17:43:58 -0300

    Add files via upload

--- taller8.sql
+++ taller8.sql
@@ -1,4 +1,4 @@
-/*CREATE TABLE transacciones(
+CREATE TABLE transacciones(
 	tabla varchar(25),
 	fecha_hora timestamp,
 	accion varchar(15),
@@ -13,13 +13,15 @@ CREATE TABLE ventasxmes(
 	transaccion VARCHAR,/*Tipo Operacion/NOMBRE */
 	cant_tr INTEGER, /*Cantidad Transacciones por mes */
 	mtotal_tr INTEGER /*El monto total que suman las transacciones del mes*/
-);*/
+);
 
 /*Funcion Punto 1*/
 CREATE OR REPLACE FUNCTION tr_pronopr() RETURNS TRIGGER AS $$
 	DECLARE	
 		newR varchar;
 		oldR varchar;
+		concatnew varchar;
+		concatold varchar;
 		i integer;
 	BEGIN
 		i := 1;
@@ -27,17 +29,31 @@ CREATE OR REPLACE FUNCTION tr_pronopr() RETURNS TRIGGER AS $$
 		oldR := '';
 		IF (TG_OP = 'UPDATE') THEN 
 			WHILE i <= TG_ARGV[0]::INTEGER LOOP
-			IF ( SPLIT_PART(NEW::varchar, ',', i) != SPLIT_PART(OLD::varchar, ',', i) ) THEN
-				IF (newR = '' AND oldR = '') THEN
-					newR := CONCAT(newR, SPLIT_PART(NEW::varchar, ', ', i));
-					oldR := CONCAT(oldR, SPLIT_PART(OLD::varchar, ', ', i));
-				ELSE
-					newR := CONCAT(newR, ' , ', SPLIT_PART(NEW::varchar, ',', i));
-					oldR := CONCAT(oldR, ' , ', SPLIT_PART(OLD::varchar, ',', i));
+				IF (SPLIT_PART(NEW::varchar, ',', i) != SPLIT_PART(OLD::varchar, ',', i)) THEN
+					IF (SPLIT_PART(NEW::varchar, ',', i) = '') THEN 
+						concatnew:= 'NULO';
+					ELSE
+						concatnew:= SPLIT_PART(NEW::varchar, ',', i);
+					END IF;
+					
+					IF(SPLIT_PART(OLD::varchar, ',', i) = '') THEN
+						concatold:= 'NULO'; 
+					ELSE 
+						concatold:= SPLIT_PART(OLD::varchar, ',', i);
+					END IF;
+					
+					IF (newR = '' AND oldR = '') THEN
+						newR := concatnew;
+						oldR := concatold;
+					ELSE
+						newR := CONCAT(newR, ' , ', concatnew);
+						oldR := CONCAT(oldR, ' , ', concatold);
+					END IF;
+					RAISE NOTICE 'newR: %', newR ;
+					RAISE NOTICE 'oldR: %', oldR ;
 				END IF;
-			END IF;
-			i := i + 1;
-		END LOOP;	
+				i := i + 1;
+			END LOOP;	
 		ELSE 
 			newR := NEW::varchar;
 			oldR := OLD::varchar;
@@ -85,10 +101,3 @@ $$ LANGUAGE plpgsql;
 /*Triggers Punto 3*/
 CREATE TRIGGER tr_opVA BEFORE INSERT OR UPDATE ON propiedades
       FOR EACH ROW EXECUTE PROCEDURE  tr_valsup() ;
-
-
-	  
-select * from provincias
-select * from propiedades WHERE provincia = 2 AND tipo_propiedad = 2  AND superficie = 180 ORDER BY id_propiedad 
-INSERT INTO propiedades(tipo_propiedad, provincia, superficie,dueno)
-VALUES (2,2, 180,'24295582-K')
