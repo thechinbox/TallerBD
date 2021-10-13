82ec12eb6a3ced8ac9758e2c4924c001dd2a49a2
thechinbox <89473920+thechinbox@users.noreply.github.com>
2021-10-13 15:10:53 -0300

    Add files via upload

--- taller8.sql
+++ taller8.sql
@@ -1,4 +1,4 @@
-CREATE TABLE transacciones(
+/*CREATE TABLE transacciones(
 	tabla varchar(25),
 	fecha_hora timestamp,
 	accion varchar(15),
@@ -13,14 +13,7 @@ CREATE TABLE ventasxmes(
 	transaccion VARCHAR,/*Tipo Operacion/NOMBRE */
 	cant_tr INTEGER, /*Cantidad Transacciones por mes */
 	mtotal_tr INTEGER /*El monto total que suman las transacciones del mes*/
-);
-
-/*Triggers Punto 1*/
-CREATE TRIGGER tr_pro AFTER INSERT OR UPDATE OR DELETE ON propiedades
-      FOR EACH ROW EXECUTE PROCEDURE  tr_pronopr(6) ;
-	  
-CREATE TRIGGER tr_op AFTER INSERT OR UPDATE OR DELETE ON operaciones
-      FOR EACH ROW EXECUTE PROCEDURE  tr_pronopr(7) ;
+);*/
 
 /*Funcion Punto 1*/
 CREATE OR REPLACE FUNCTION tr_pronopr() RETURNS TRIGGER AS $$
@@ -55,22 +48,47 @@ CREATE OR REPLACE FUNCTION tr_pronopr() RETURNS TRIGGER AS $$
 	END	
 $$ LANGUAGE plpgsql;
 
-/*Triggers Punto 3*/
-CREATE TRIGGER tr_opVA BEFORE INSERT OR UPDATE ON propiedades
-      FOR EACH ROW EXECUTE PROCEDURE  tr_valsup() ;
+/*Triggers Punto 1*/
+CREATE TRIGGER tr_pro AFTER INSERT OR UPDATE OR DELETE ON propiedades
+      FOR EACH ROW EXECUTE PROCEDURE  tr_pronopr(6) ;
 	  
+CREATE TRIGGER tr_op AFTER INSERT OR UPDATE OR DELETE ON operaciones
+      FOR EACH ROW EXECUTE PROCEDURE  tr_pronopr(7) ;
+
+/*Funcion Punto 3*/
 CREATE OR REPLACE FUNCTION tr_valsup() RETURNS TRIGGER AS $$
+	DECLARE 
+	prov varchar;
+	tipopr varchar;
 	BEGIN
-		IF (NEW.provincia = 'Lleida' AND NEW.tipo_propiedad = 'Casa') THEN
-			
-		ELSEIF (NEW.Provincia = 'Taragona' AND NEW.tipo_propiedad = 'Casa') THEN
-			
-		ELSEIF (NEW.Provincia = 'Girona' AND NEW.tipo_propiedad = 'Suelo') THEN
-			
-		ELSEIF (NEW.Provincia = 'Barcelona') THEN
+		prov = (SELECT provincia FROM provincias WHERE id_provincia = NEW.provincia);
+		tipopr = (SELECT tipo_propiedad FROM tipos_propiedades WHERE id_tipo = NEW.tipo_propiedad);
 		
-		ELSEIF (NEW.tipo_propiedad = 'Industrial') THEN
+		IF (prov = 'LLEIDA' AND tipopr ='PARKING' AND NEW.superficie < 100) THEN
+			RAISE EXCEPTION 'Superficie Menor a 100';
 			
+		ELSEIF (prov = 'TARAGONA' AND tipopr = 'CASA' AND NEW.superficieconstruida > 3 * NEW.superficie) THEN
+				RAISE EXCEPTION 'Superficie Construida Supera el Limite Establecido';
+				
+		ELSEIF (prov = 'GIRONA' AND tipopr = 'SUELO' AND NEW.superficie < 200) THEN
+			RAISE EXCEPTION 'Superficie Menor a 200';
+
+		ELSEIF (prov = 'BARCELONA' AND NEW.superficieconstruida > 2 * NEW.superficie) THEN
+			RAISE EXCEPTION 'Superficie Menor a 200';
+			
+		ELSEIF (tipopr = 'INDUSTRIAL' AND NEW.superficie < 500) THEN
+			RAISE EXCEPTION 'Superficie Menor a 500';
 		END IF;
 	END
 $$ LANGUAGE plpgsql;
+
+/*Triggers Punto 3*/
+CREATE TRIGGER tr_opVA BEFORE INSERT OR UPDATE ON propiedades
+      FOR EACH ROW EXECUTE PROCEDURE  tr_valsup() ;
+
+
+	  
+select * from provincias
+select * from propiedades WHERE provincia = 2 AND tipo_propiedad = 2  AND superficie = 180 ORDER BY id_propiedad 
+INSERT INTO propiedades(tipo_propiedad, provincia, superficie,dueno)
+VALUES (2,2, 180,'24295582-K')
